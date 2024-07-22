814dd3f18dabdb485062217a08d3e481bf6fc2d2
Ibrahim Naeem <ibrahim@naeem.mv>
2024-07-22 13:49:15 +0500

    override client and transactions classes

--- src/Client.php
+++ src/Client.php
@@ -2,13 +2,53 @@
 
 namespace Naeemmv\BmlConnect;
 
-use BMLConnect\Client as BMLClient;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response;
 
-class Client extends BMLClient
+class Client
 {
-    const DEV_ENDPOINT = 'https://pay.naeem.mv/';
+    const DEV_ENDPOINT = 'https://pay.naeem.mv/api/';
+    const BML_API_VERSION = '2.0';
+    const BML_APP_VERSION = 'bml-connect-php';
+    const BML_SIGN_METHOD = 'sha1';
+    const BML_SANDBOX_ENDPOINT = 'https://api.uat.merchants.bankofmaldives.com.mv/public/';
+    const BML_PRODUCTION_ENDPOINT = 'https://api.merchants.bankofmaldives.com.mv/public/';
 
-    private string $baseUrl;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $httpClient;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var string
+     */
+    private $appId;
+
+    /**
+     * @var string
+     */
+    private $mode;
+
+    /**
+     * @var array
+     */
+    private $clientOptions;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @var Transactions
+     */
+    public $transactions;
+
 
     /**
      * Client constructor.
@@ -19,8 +59,122 @@ class Client extends BMLClient
      */
     public function __construct(string $apiKey, string $appId, $mode = 'production', array $clientOptions = [])
     {
-        parent::__construct($apiKey, $appId, $mode, $clientOptions);
-
+        $this->apiKey = $apiKey;
+        $this->appId = $appId;
+        $this->mode = $mode;
+        $this->baseUrl = ($mode === 'production' ? self::BML_PRODUCTION_ENDPOINT : self::BML_SANDBOX_ENDPOINT);
         $this->baseUrl = ($mode === 'development') ? self::DEV_ENDPOINT : $this->baseUrl;
+        $this->clientOptions = $clientOptions;
+
+        $this->initiateHttpClient();
+
+        $this->transactions = new Transactions($this);
+    }
+
+    /**
+     * @param GuzzleClient $client
+     */
+    public function setClient(GuzzleClient $client)
+    {
+        $this->httpClient = $client;
+    }
+
+    /**
+     * Initiates the HttpClient with required headers
+     */
+    private function initiateHttpClient()
+    {
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' =>  $this->apiKey,
+            ]
+        ];
+
+        $this->httpClient = new GuzzleClient(array_replace_recursive($this->clientOptions, $options));
+    }
+
+    private function buildBaseUrl()
+    {
+        return $this->baseUrl;
+    }
+
+    /**
+     * @param Response $response
+     * @return mixed
+     */
+    private function handleResponse(Response $response)
+    {
+        $stream = \GuzzleHttp\Psr7\Utils::streamFor($response->getBody());
+        $data = json_decode($stream);
+
+        return $data;
+    }
+
+    /**
+     * @param string $endpoint
+     * @param array $json
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function post($endpoint, $json)
+    {
+        $json['apiVersion'] = self::BML_API_VERSION;
+        $json['appVersion'] = self::BML_APP_VERSION;
+        $json['signMethod'] = self::BML_SIGN_METHOD;
+
+        $response = $this->httpClient->request('POST', $this->buildBaseUrl().$endpoint, ['json' => $json]);
+        return $this->handleResponse($response);
+    }
+
+    /**
+     * @param string $endpoint
+     * @param array $pagination
+     * @return mixed
+     */
+    public function get(string $endpoint, array $pagination = [])
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            $this->applyPagination($this->buildBaseUrl().$endpoint, $pagination)
+        );
+
+        return $this->handleResponse($response);
+    }
+
+    /**
+     * @param string $url
+     * @param array $pagination
+     * @return string
+     */
+    private function applyPagination(string $url, array $pagination)
+    {
+        if (count($pagination)) {
+            return $url.'?'.http_build_query($this->cleanPagination($pagination));
+        }
+
+        return $url;
+    }
+
+    /**
+     * @param array $pagination
+     * @return array
+     */
+    private function cleanPagination(array $pagination)
+    {
+        $allowed = [
+            'page',
+        ];
+
+        return array_intersect_key($pagination, array_flip($allowed));
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
     }
 }
